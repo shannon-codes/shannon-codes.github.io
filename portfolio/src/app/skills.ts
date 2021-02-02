08233bebf66ef8b09f13d14e67151a937a2f6c84
shannon-codes <shannonlim8@gmail.com>
2021-02-02 11:57:00 -0500

    alternate direction project tiles, fix mobile responsiveness

--- portfolio/src/app/skills.ts
+++ portfolio/src/app/skills.ts
@@ -1,26 +1,26 @@
 export const skills = [
   {
-    icon:'../../assets/images/frontend.svg',
+    icon:'../../assets/images/icon5.svg',
     name: 'Front-end Programming',
     tech: 'Angular 10, React, JavaScript, ES6, Bootstrap, HTML5, CSS3, Parse JSON'
   },
   {
-    icon:'../../assets/images/backend.svg',
+    icon:'../../assets/images/icon10.svg',
     name: 'Back-end Programming',
     tech: 'Java (Spring Boot, Hibernate ORM, JUnit Test), C# .NET, RESTful API and Web Services return JSON, Linux Bash Shell'
   },
   {
-    icon:'../../assets/images/mobile.svg',
+    icon:'../../assets/images/icon2.svg',
     name: 'Mobile Application',
     tech: 'Android Kotlin (Android Studio IDE), iOS Swift (XCode IDE), SQLite3'
   },
   {
-    icon:'../../assets/images/database.svg',
+    icon:'../../assets/images/icon13.svg',
     name: 'Database',
     tech: 'SQL Server Management Studio 17, MySQL 5.7, Oracle SQL Developer, Big Data Storage, MongoDB NoSQL'
   },
   {
-    icon:'../../assets/images/analysis.svg',
+    icon:'../../assets/images/business.svg',
     name: 'System Analysis',
     tech: 'Software Development Life Cycle (Plan, Analyze, Design, Implement, Test, Maintain), Diagram (System Context Diagram, Data Flow Diagrams), Microsoft Visio'
   },
