6a6b43ec99e8728d89a1b98492c4392efeb738e8
shannon-codes <shannonlim8@gmail.com>
2021-02-14 15:18:45 -0500

    updated colors, and style heights

--- portfolio/src/app/projects.ts
+++ portfolio/src/app/projects.ts
@@ -3,19 +3,22 @@ export const projects = [
     id: 1,
     name: 'Yoga Learning Assistant',
     image: '../../assets/images/c1.png',
-    description: 'The Yoga Learning Assistant is a web platform that provides real-time feedback on live webcam capture. The application live detects 17 body joints of the user in 2D space and compares it to the position of the instructor on the yoga video. This was the Capstone Winner for the 2020 Sheridan Applied Computing Capstone Showcase Competition.',
-    tech: 'React, PoseNet, TensorFlow.js, Bootstrap, Node.js, MongoDB',
-    demo: 'http://capstone.dev.fast.sheridanc.on.ca/grpTechCatDream/videos/grpTechCatDream_AI_Yoga_Learning_Assistant_Pitch.mp4'
+    description: 'The Yoga Learning Assistant is a web platform that provides real-time feedback on live webcam capture. The application live detects 17 body joints of the user in 2D space and compares it to the position of the instructor on the yoga video. ',
+    tech: 'React · PoseNet · TensorFlow.js · React · Bootstrap · Node.js · MongoDB',
+    video: 'http://capstone.dev.fast.sheridanc.on.ca/grpTechCatDream/videos/grpTechCatDream_AI_Yoga_Learning_Assistant_Pitch.mp4',
+    link: 'http://capstone.dev.fast.sheridanc.on.ca/grpTechCatDream/',
+    awards:[' 1st Place SDNE Capstone Winner', ' 1st Place Sheridan Student Experience Award']
   },
   {
     id: 2,
     name: 'Uplift',
     image: '../../assets/images/uplift.png',
     github: 'https://github.com/shannon-codes/rbcuplift',
-    description: 'Designed this website at the RBC AmpHacks 2020 36-hour hackathon for 3rd/4th year students. Our four-member team won 2nd place. RBC Small business owners get their questions answered and brainstorm over 1-hour calls with mentors from departments like technology, marketing, human resources, and management. Uplift platform helps build customer relationships and helps businesses during the pandemic.',
-    tech: 'Angular',
+    description: 'Designed this website at an RBC hackathon and worked with new people. The purpose is for RBC Small business owners to connect with free mini-mentorships for brainstorming over 1-hour calls. RBC mentors are experts beyond banking, coming from departments of technology, marketing, human resources, and management.',
+    tech: 'Angular 8 · TypeScript · Routing · Bootstrap · CSS',
     document:'../../assets/images/Uplift.pdf',
-    demo: 'https://shannon-codes.github.io/rbcuplift'
+    demo: 'https://shannon-codes.github.io/rbcuplift',
+    awards: [' 2nd place RBC AmpHacks 2020 Competition']
   },
   {
     id: 3,
@@ -23,7 +26,7 @@ export const projects = [
     image: '../../assets/images/MovieNight.png',
     github: 'https://github.com/shannon-codes/iOS-Movie-Night',
     description: 'An iOS mobile app that helps users plan their movie night. The application fetches movie information from themoviedb.org API. Users can add, view, save, and delete their list of Favorites and set notifications for movie release dates. More screenshots and details in README on GitHub.',
-    tech: 'Swift, SQLite3, Firebase, CoreLocation, Xcode',
+    tech: 'Swift · SQLite3 · Firebase · CoreLocation · Xcode',
     document: '../../assets/images/iOS Movie Night App.pdf'
   }, 
   {
@@ -32,7 +35,7 @@ export const projects = [
     image: '../../assets/images/GradesManagement.png',
     github: 'https://github.com/shannon-codes/student-grades-portal',
     description: 'Web application to organize student grades using Java Spring Boot. The professor role has CRUD functionality to organize students and their grades, and can see the class overall average, and average by each assessment category. User passwords are salted before storing in database. Uses in-memory H2 database or a local MySQL database. Roles are assigned so a student roles is denied access to the Professor area using Spring Security. File structure using MVC pattern.',
-    tech: 'Java Spring Boot, H2 database or MySQL workbench, JPA, ThymeLeaf, Bootstrap, CSS'
+    tech: 'Java Spring Boot · H2 database · MySQL · JPA · ThymeLeaf · Bootstrap · CSS'
   },
   {
     id: 5,
@@ -40,7 +43,7 @@ export const projects = [
     image: '../../assets/images/eswap.JPG',
     github: 'https://github.com/shannon-codes/Community-eSwap',
     description: 'This web application is for current computer science students to volunteer with community non-profits. Non-profit community organization or individuals can post opportunities for students. Organizations swap assets such as rehearsal venue, instruments, use of truck, etc, if monetary payment is not available',
-    tech: 'C# ASP.NET, Microsoft SQL Server'
+    tech: 'C# ASP.NET · Microsoft SQL Server'
   },
   {
     id: 6,
@@ -48,7 +51,7 @@ export const projects = [
     image: '../../assets/images/Heuristic1.png',
     github: 'https://github.com/shannon-codes/Knights-Tour',
     description: 'This application runs simulations of the Knight Tour problem. The aim is the move the knight to every position on the chessboard, but the knight can visit a square only once. It allows the user to choose the simulation method and the starting position. The result displays the sequence of movements of the knight on the chessboard and the total number of squares visited.',
-    tech: 'C# .NET, Windows Forms'
+    tech: 'C# .NET · Windows Forms'
   },
   {
     id: 7,
@@ -56,6 +59,6 @@ export const projects = [
     image: '../../assets/images/HealthDiary.JPG',
     github: 'https://github.com/shannon-codes/HealthDiary',
     description: 'This application allows users to complete a daily questionnaire entry on health-related metrics such as glasses of water, meals, hours of sleep, calories burned, amount of exercise, etc. It allows the user to create, read, update, delete, and search for diary entries. It saves entries to an output file.',
-    tech: 'Java, JavaFX, SceneBuilder'
+    tech: 'Java · JavaFX · SceneBuilder'
   }
 ];
--- portfolio/src/app/skills.ts
+++ portfolio/src/app/skills.ts
@@ -2,26 +2,26 @@ export const skills = [
   {
     icon:'../../assets/images/icon5.svg',
     name: 'Front-end Programming',
-    tech: 'Angular 10, React, JavaScript, ES6, Bootstrap, HTML5, CSS3, Parse JSON'
+    tech: ['Angular 10 (TypeScript)', 'React', 'JavaScript ES6', 'HTML5, CSS3, Bootstrap', 'Consume API, Parse JSON']
   },
   {
     icon:'../../assets/images/icon10.svg',
     name: 'Back-end Programming',
-    tech: 'Java (Spring Boot, Hibernate ORM, JUnit Test), C# .NET, RESTful API and Web Services return JSON, Linux Bash Shell'
+    tech: ['Java (Spring Boot, Hibernate ORM, JUnit Test)',' C# .NET', 'RESTful API and Web Services return JSON', 'Linux Bash Shell']
   },
   {
     icon:'../../assets/images/icon2.svg',
     name: 'Mobile Application',
-    tech: 'Android Kotlin (Android Studio IDE), iOS Swift (XCode IDE), SQLite3'
+    tech: ['Android Kotlin (Android Studio IDE)', 'iOS Swift (XCode IDE)', 'SQLite3']
   },
   {
     icon:'../../assets/images/icon13.svg',
     name: 'Database',
-    tech: 'SQL Server Management Studio 17, MySQL 5.7, Oracle SQL Developer, Big Data Storage, MongoDB NoSQL'
+    tech: ['SQL Server Management Studio 17', 'MySQL 5.7', 'Oracle SQL Developer', 'Big Data Storage', 'MongoDB NoSQL']
   },
   {
     icon:'../../assets/images/business.svg',
     name: 'System Analysis',
-    tech: 'Software Development Life Cycle (Plan, Analyze, Design, Implement, Test, Maintain), Diagram (System Context Diagram, Data Flow Diagrams), Microsoft Visio'
+    tech: ['Software Development Life Cycle', 'System Context Diagram', 'Data Flow Diagrams', 'Microsoft Visio']
   },
 ];
